691618003cae397087965e8d91261869e27b51ab
Arjun-1998 <arjunjaiswal8065@gmail.com>
2021-12-10 00:01:46 +0530

    [Arjun] Add ; Added Switched

---
+++ DataStructures/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List operations");
+            LinkedList List = new LinkedList();
+            List.Add(56);
+            List.Add(30);
+            List.Add(70);
+            List.Display();
+        }
+    }
+}
